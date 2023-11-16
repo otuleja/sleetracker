99d13dcc95a8b05502e072bc6c378db3d780906b
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-11-16 21:23:39 +0100

    sleepiness.page.html and remove css from sleepiness.page.ts

--- sleeptracker/src/app/modals/sleepiness/sleepiness.page.ts
+++ sleeptracker/src/app/modals/sleepiness/sleepiness.page.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sleepiness',
   templateUrl: './sleepiness.page.html',
-  styleUrls: ['./sleepiness.page.scss'],
+  // styleUrls: ['./sleepiness.page.scss'],
 })
 export class SleepinessPage implements OnInit {
 
