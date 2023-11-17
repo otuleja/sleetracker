4a86f1609446d255a358844409d2d487fa15ec82
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-11-17 19:51:29 +0100

    change to ts congif, add capacitor preferences, save data - MAKE SURE YOU COMMENT OUT DEFAULT DATA

--- sleeptracker/src/app/services/sleep.service.ts
+++ sleeptracker/src/app/services/sleep.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
-
+// import { Plugins } from '@capacitor/core';
+// const { Storage } = Plugins;
+import { Preferences } from '@capacitor/preferences';
 @Injectable({
 	providedIn: 'root'
 })
@@ -20,14 +22,44 @@ export class SleepService {
 	}
 
 	private addDefaultData() {
-		this.logOvernightData(new OvernightSleepData(new Date('February 18, 2021 01:03:00'), new Date('February 18, 2021 09:25:00')));
-		this.logSleepinessData(new StanfordSleepinessData(4, new Date('February 19, 2021 14:38:00')));
-		this.logOvernightData(new OvernightSleepData(new Date('February 20, 2021 23:11:00'), new Date('February 21, 2021 08:03:00')));
+		// this.logOvernightData(new OvernightSleepData(new Date('February 18, 2021 01:03:00'), new Date('February 18, 2021 09:25:00')));
+		// this.logSleepinessData(new StanfordSleepinessData(4, new Date('February 19, 2021 14:38:00')));
+		// this.logOvernightData(new OvernightSleepData(new Date('February 20, 2021 23:11:00'), new Date('February 21, 2021 08:03:00')));
+
+		Preferences.keys().then((data) => {
+			console.log("data from load", data)
+			// data.keys.forEach(x => {
+			//   Storage.get({key: x}).then((result) =>{
+			//     var dt = JSON.parse(result.value);
+			//     if (dt.titleString === "Overnight Sleep"){
+			//       var temp = new OvernightSleepData(new Date(dt.sleepStart), new Date(dt.sleepEnd));
+			//       console.log(temp.sortDate);
+			//       this.allSleepData.push(temp);
+			//     } else {
+			//       var tmp = new StanfordSleepinessData(dt.loggedValue, new Date(dt.loggedAt));
+			//       console.log(tmp.sortDate);
+			//       this.allSleepData.push(tmp);
+			//     }
+
+			//   })
+		});
 	}
 
 	public logOvernightData(sleepData: OvernightSleepData) {
+		console.log("adding sleep data", sleepData)
 		SleepService.AllSleepData.push(sleepData);
 		SleepService.AllOvernightData.push(sleepData);
+		Preferences.set({
+			key: sleepData.id, value: JSON.stringify({
+				loggedAt: sleepData.loggedAt.toISOString(),
+				sleepEnd: sleepData.sleepEnd.toISOString(),
+				sleepStart: sleepData.sleepStart.toISOString(),
+			})
+		}).then(_ => {
+			console.log("stored data");
+		}).catch(err => {
+			console.log("errrrr", err)
+		});
 	}
 
 	public logSleepinessData(sleepData: StanfordSleepinessData) {
