39d275a468729b0e7e47298063d7da434e2edc2c
Owen Tuleja <otuleja@Owens-MacBook-Pro.local>
2023-11-17 12:53:44 +0100

    copy sleepiness modal to overnight modal

---
+++ sleeptracker/src/app/modals/overnight/sleepiness.page.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { SleepService } from '../../services/sleep.service';
+// import { SleepData } from '../data/sleep-data';
+// import { OvernightSleepData } from '../data/overnight-sleep-data';
+import { StanfordSleepinessData } from '../../data/stanford-sleepiness-data';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+// import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-sleepiness',
+  templateUrl: './sleepiness.page.html',
+  // styleUrls: ['./sleepiness.page.scss'],
+})
+export class SleepinessPage implements OnInit {
+
+  level: number = 1;
+
+
+  constructor
+    (private sleepService: SleepService, private route: Router,
+      private modalController: ModalController
+      // public toastController: ToastController
+    ) { }
+  ngOnInit() { }
+
+  // async presentToast() {
+  //   const toast = await this.toastController.create({
+  //     message: 'Your sleepiness level has been recorded!',
+  //     duration: 2000
+  //   });
+  //   toast.present();
+  // 	}
+
+  dismissModal() {
+    console.log("should dismiss")
+    return this.modalController.dismiss(null, 'manual');
+  }
+  // 	// Record value once user prompts
+  recordValue() {
+    // Create new data of type StanfordSleepinessData and add to 
+    this.sleepService.logSleepinessData(new StanfordSleepinessData(this.level, new Date()));
+    // console.log(this.allSleepData);
+    // this.presentToast();
+    return this.modalController.dismiss(null, 'automatic');
+
+    // Go back to home page and display modal to confirm success
+    // this.route.navigate(['/home']);
+
+  }
+
+  // get allSleepData() {
+  // 	return SleepService.AllSleepData;
+  // }
+}
